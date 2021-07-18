e3e62a1bc74c77855a5557fe7eac9c5b5c952d0e
TsuHoppin <Johnsalvato5@gmail.com>
2021-07-18 16:24:19 -0400

    add game mechanics

--- assets/js/game.js
+++ assets/js/game.js
@@ -20,9 +20,26 @@ enemyHealth = enemyHealth - playerAttack
   // Log a resulting message to the console so we know that it worked.
 console.log(
     playerName +  " attacked " + enemyName + "." + enemyName + " now has " + enemyHealth + " health remaining "
+    
 );
+
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " Has died!");
+}
+
+else{
+    window.alert( enemyName + " still has " + enemyHealth + " health left ")
+};
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
 playerHealth = playerHealth - enemyAttack
   // Log a resulting message to the console so we know that it worked.
  console.log( enemyName + " attacked " + playerName + "." + playerName + " now has " + playerHealth + " health remaining "
  );
+// check to see if the value of the playerHealth variable is greater than 0
+if (playerHealth <= 0){
+    console.log("Your Player is still alive!")
+}
+    else {
+      window.alert("playerName" + " Has Died ");
+    }
+  
